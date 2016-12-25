51893e2e249f1c6f4f49674d27f81585a6143db1
Raphael Alves <rapha.tavares1010@gmail.com>
2016-12-25 10:45:07 -0200

    menu criado
    separando o menu para um funcionamento mais dinâmico :)

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,7 @@ Class Home extends CI_Controller{
 	function index(){
 
 		$this->load->view('template/header');
+		$this->load->view('template/menu');
 		$this->load->view('home');
 		$this->load->view('template/footer');
 	}
